e3832d2a6b9d5232ff9ed09d24520d409fb59a91
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-05-08 15:12:15 +0200

    initial commit with array in server.php

---
+++ server.php
@@ -0,0 +1,18 @@
+<?php 
+
+$dischi = [
+    [
+        "title" => "New Jersey",
+        "author" => "Bon Jovi",
+        "year" => 1988,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg",
+        "genre" => "Rock"
+    ],
+    [
+        "title" => "Live at Wembley 86",
+        "author" => "Queen",
+        "year" => 1992,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg",
+        "genre" => "Pop"
+    ]
+]
